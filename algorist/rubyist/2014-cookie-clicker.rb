ac7fc2426905a39ab0de686498571fbe9cae3295
henry4j <henry4js@gmail.com>
2014-04-12 21:46:06 -0700

    .

--- algorist/rubyist/2014-cookie-clicker.rb
+++ algorist/rubyist/2014-cookie-clicker.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/env /usr/local/bin/jruby
 
-%w{test/unit}.each { |e| require e }
-
 module CodeJam
   def self.main(io)
     cases = 1.upto(io.readline.to_i).map do |tc|
@@ -15,7 +13,7 @@ module CodeJam
     e = 0.0
     g = 2.0
     m = x/g
-    while c/g > 1e-6
+    while c/g > 1e-6 && g < x
       m = e + x/g if e + x/g < m
       e += c/g
       g += f
@@ -24,10 +22,16 @@ module CodeJam
   end
 end
 
-class TestCases < Test::Unit::TestCase
-  def test_main
-    system 'curl -o /tmp/small.in -kL https://raw.githubusercontent.com/henry4j/-/master/algorist/rubyist/cookie-clicker-testcases/small.in' unless File.exists? '/tmp/small.in'
-    test_case_uri = '/tmp/small.in'
-    open(test_case_uri) { |io| CodeJam.main(io) }
+if ENV['DBGP_RUBY_PORT']
+  require 'test/unit'
+
+  class TestCases < Test::Unit::TestCase
+    def test_main
+      system 'curl -o /tmp/small.in -kL https://raw.githubusercontent.com/henry4j/-/master/algorist/rubyist/cookie-clicker-testcases/small.in' unless File.exists? '/tmp/small.in'
+      test_case_uri = '/tmp/small.in'
+      open(test_case_uri) { |io| CodeJam.main(io) }
+    end
   end
+else
+  CodeJam.main(ARGF)
 end
