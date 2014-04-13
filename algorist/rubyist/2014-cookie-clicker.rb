b84f30205c9e3c83d7521ea7880e51f569999510
henry4j <henry4js@gmail.com>
2014-04-12 18:32:42 -0700

    .

--- algorist/rubyist/2014-cookie-clicker.rb
+++ algorist/rubyist/2014-cookie-clicker.rb
@@ -4,29 +4,31 @@
 # %w{open-uri}.each { |e| require e }
 
 module CodeJam
-#  def self.main(io)
-#    cases = 1.upto(io.readline.to_i).map do |tc|
-#      r, c, m = io.readline.chomp.split.map { |e| e.to_i }
-#      [tc, r, c, m]
-#    end
-#    cases.each { |e| puts draw(*e) }
-#  end
+  def self.main(io)
+    cases = 1.upto(io.readline.to_i).map do |tc|
+      c, f, x = io.readline.chomp.split.map { |e| e.to_f }
+      p c, f, x
+      [tc, c, f, x]
+    end
+    cases.each { |e| puts solve(*e) }
+  end
 
   def self.solve(tc, c, f, x)
     max = x/2 # 50
     map = lambda do |e, g|
+      puts g/f
       a = x/g
-      if e + c/g > max
+      if e + a > max
         max
       elsif a < c/g
         e + a
       else
         b = map.call(e + c/g, g+f)
         [e + a, b].min
-      end
+      end # memo?
     end
     s = map.call(0, 2.0)
-    puts "Case ##{tc}: #{s}\n"
+    "Case ##{tc}: #{s}"
   end
 end
 
@@ -34,8 +36,9 @@ end
 
 class TestCases < Test::Unit::TestCase
   def test_main
-    CodeJam.solve(2, 500.0, 4.0, 2000.0)
-    # test_case_uri = 'https://raw.githubusercontent.com/henry4j/-/master/algorist/rubyist/minesweeper-testcases/small.in'
-    # open(test_case_uri) { |io| CodeJam.solve(2, 30.0, 2.0, 100.0) }
+    
+    CodeJam.solve(4, 30.50000, 3.14159, 1999.19990)
+#    test_case_uri = '/workspace/gits/henry4j/algorist/rubyist/cookie-clicker-testcases/small.in'
+#    open(test_case_uri) { |io| CodeJam.main(io) }
   end
 end
