73a23ebae4074c8e52fbd7d1904fdbe11590ea0e
henry4j <henry4js@gmail.com>
2014-05-16 14:54:13 -0700

    .

--- streams/src/test/java/com/henry4j/MyTest.java
+++ streams/src/test/java/com/henry4j/MyTest.java
@@ -1,9 +1,17 @@
 package com.henry4j;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import com.google.common.base.Joiner;
+import com.google.common.primitives.Ints;
 
 public class MyTest {
-	
+    @Test
+    public void test() {
+        int[] ints = { 1, 2, 3, 4 };
+        int[] even = Arrays.stream(ints).filter(i -> i % 2 == 0).toArray();
+        System.out.println(Joiner.on(", ").join(Ints.asList(even)));
+    }
 }
