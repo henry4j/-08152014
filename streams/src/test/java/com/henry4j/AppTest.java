c6e8ba6610ea73e8a190e34285b7dc54e67511dd
henry4j <henry4js@gmail.com>
2014-05-16 18:54:32 -0700

    .

--- streams/src/test/java/com/henry4j/AppTest.java
+++ streams/src/test/java/com/henry4j/AppTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import com.google.common.base.Joiner;
 import com.google.common.primitives.Ints;
+import com.sun.istack.internal.NotNull;
 
 public class AppTest {
     @Test
@@ -13,5 +14,9 @@ public class AppTest {
         int[] ints = { 1, 2, 3, 4 };
         int[] even = Arrays.stream(ints).filter(i -> i % 2 == 0).toArray();
         System.out.println(Joiner.on(", ").join(Ints.asList(even)));
+    }
+
+    public static class Edge {
+        
     }
 }
