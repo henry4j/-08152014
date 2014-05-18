47e5a3d2d88619991ae11e23ba3bc10d8f85a1be
henry4j <henry4js@gmail.com>
2014-05-17 22:20:39 -0700

    .

--- rubyist/rubyist.rb
+++ rubyist/rubyist.rb
@@ -1,6 +1,94 @@
 #!/usr/bin/env ruby
 
+class Graph
+  def self.dijkstra(s, each_vertex, each_edge)
+    parents = {}
+    distances = Hash.new(Float::MAX).merge(s => 0)
+    q = BinaryHeap.new(lambda { |a, b| a[1] <=> b[1] }, lambda { |e| e[0] }) # e[0] has v; [1] has a distance.
+    each_vertex.call(lambda { |v| q.offer([v, Float::MAX]) })
+    q.offer([s, 0])
+    until q.empty? || Float::MAX == q.peek[1]
+      each_edge[u = q.poll[0], lambda do |v, w|
+        via_u = distances[u] + w
+        if via_u < distances[v]
+          q.offer([v, distances[v] = via_u])
+          parents[v] = u
+        end
+      end]
+    end
+    parents
+  end
+end
+
 class BinaryHeap # min-heap by default, http://en.wikipedia.org/wiki/Binary_heap
+  # http://docs.oracle.com/javase/8/docs/api/java/util/PriorityQueue.html
+  # a binary heap is a complete binary tree, where all levels but the last one are fully filled, and
+  # each node is smaller than or equal to each of its children according to a comparer specified.
+  # In Java, new PriorityQueue<Node>(capacity, (a, b) -> a.compareTo(b));
+  def initialize(comparer = lambda { |a, b| a <=> b }, hash = lambda { |e| e.hash }) # min-heap by default
+    @a = []
+    @h = {}
+    @comparer = comparer
+    @hash = hash
+  end
+
+  def offer(e)
+    n = @h[@hash[e]]
+    if n
+      @a[n] = e
+      if n == bubble_up(n)
+        bubble_down(n)
+      end
+    else
+      @a << e
+      bubble_up(@a.size - 1)
+    end
+    self # works as a fluent interface.
+  end
+
+  def peek
+    @a[0]
+  end
+
+  def poll
+    unless @a.empty?
+      @a[0], @a[-1] = @a[-1], @a[0]
+      head = @a.pop
+      bubble_down(0) unless @a.empty?
+      head
+    end
+  end
+
+  def bubble_up(n)
+    if n > 0 && @comparer.call(@a[p = (n-1)/2], @a[n]) > 0
+      @a[p], @a[n] = @a[n], @a[p]
+      @h[@hash[@a[n]]] = n
+      bubble_up(p)
+    else
+      @h[@hash[@a[n]]] = n
+    end
+  end
+
+  def bubble_down(n)
+    c = [n]
+    c << 2*n + 1 if 2*n + 1 < @a.size
+    c << 2*n + 2 if 2*n + 2 < @a.size
+    c = c.min { |a,b| @comparer.call(@a[a], @a[b]) }
+    if c != n
+      @a[n], @a[c] = @a[c], @a[n]
+      @h[@hash[@a[n]]] = n
+      bubble_down(c)
+    else
+      @h[@hash[@a[n]]] = n
+    end
+  end
+
+  def empty?() @a.empty? end
+  def size() @a.size end
+  def to_a() @a end
+end
+
+class SimpleBinaryHeap # min-heap by default, http://en.wikipedia.org/wiki/Binary_heap
   # http://docs.oracle.com/javase/7/docs/api/java/util/PriorityQueue.html
   # a binary heap is a complete binary tree, where all levels but the last one are fully filled, and
   # each node is smaller than or equal to each of its children according to a comparer specified.
