9525b07c05897eba2fd926db4c2c62449b2203f6
henry4j <henry4js@gmail.com>
2014-06-05 23:24:13 -0700

    .

--- rubyist/1crammed.rb
+++ rubyist/1crammed.rb
@@ -2120,7 +2120,7 @@ class TestCases < Test::Unit::TestCase
 
 # 18_1 Write a method that adds two numbers. You should not use the addition (+) arithmetic operator.
 # 18_2 Write a method to shuffle a deck of cards. Each of the 52! permutations of the deck has to be equally probable.
-# 18_3 Write a method to randomly generate a set of m integers from an array of size n. Each element must have equal probability of being chosen.
+# 18_3 Write a method to randomly sample a set of m integers from an array of size n. Each element must have equal probability of being chosen.
 # 18_4 Write a function to count the number of 2s that appear in all the numbers between 0 and n (inclusive), e.g., input: 25, output: 9 (2, 12, 20, 21, 22, 23, 24, and 25); note that 22 counts for two 2s.
 # 18_7 Given a list of words, write a program that returns the longest word made of other words., e.g., return "doityourself" given a list, "doityourself", "do", "it", "yourself", "motherinlaw", "mother", "in", "law".
 # 18_8 Given a string s and an array of smaller strings Q, write a program to search s for each small string in Q.
@@ -2344,6 +2344,22 @@ class TestCases < Test::Unit::TestCase
     assert_equal "abc", longest_common_substring.call('abcabcbb')
   end
 
+  def test_18_7_find_longest_compound_words
+    w = %w(approximation do it yourself doityourself motherinlaw mother in law).sort_by { |s| -s.size }
+    d = w.each_with_object({}) { |e, d| d[e] = true }
+    s = w.detect do |word|
+      (2..3).any? do |k|
+        comositions = Partitions.compose(word.size, k)
+        Partitions.compose(word.size, k).any? do |composition|
+          prefix_sums = composition.reduce([]) { |a, e| a << e + (a[-1] || 0) }
+          words = (0...prefix_sums.size).map { |j| word[(j > 0 ? prefix_sums[j-1] : 0)...prefix_sums[j]] }
+          words.all? { |k| d.has_key?(k) }
+        end
+      end
+    end
+    assert_equal 'doityourself', s
+  end
+
   def test_18_5_smallest_snippet_of_k_words
     # Given a search string of three words, find the smallest snippet of the document that contains all three of 
     # the search words --- i.e., the snippet with smallest number of words in it. You are given the index positions 
@@ -2366,22 +2382,6 @@ class TestCases < Test::Unit::TestCase
 
     assert_equal [117, 130], min_window.call([[0, 89, 130], [95, 123, 177, 199], [70, 105, 117]])
     # assert_equal 'adab', min_window_string('abracadabra', 'abad')
-  end
-
-  def test_18_7_find_longest_compound_words
-    w = %w(approximation do it yourself doityourself motherinlaw mother in law).sort_by { |s| -s.size }
-    d = w.each_with_object({}) { |e, d| d[e] = true }
-    s = w.detect do |word|
-      (2..3).any? do |k|
-        comositions = Partitions.compose(word.size, k)
-        Partitions.compose(word.size, k).any? do |composition|
-          prefix_sums = composition.reduce([]) { |a, e| a << e + (a[-1] || 0) }
-          words = (0...prefix_sums.size).map { |j| word[(j > 0 ? prefix_sums[j-1] : 0)...prefix_sums[j]] }
-          words.all? { |k| d.has_key?(k) }
-        end
-      end
-    end
-    assert_equal 'doityourself', s
   end
 
   def test_18_4_count_twos
